60af07fe4e82d28a5045fa39fa6a4407865f035b
Jakub Godawa <jakub.godawa@gmail.com>
2018-08-11 13:49:16 +0200

    Conform style to the rest of the page

--- spec/requests/comments_ranking_spec.rb
+++ spec/requests/comments_ranking_spec.rb
@@ -5,7 +5,7 @@ describe "Comments ranking", type: :request do
   it "shows the ranking link and page" do
     visit '/'
     click_link 'Comments ranking'
-    expect(page).to have_css('h1', count: 1, text: 'Top Commentators')
+    expect(page).to have_css('h1', count: 1, text: 'Top 10 commentators')
   end
 
   it "shows top 10 commentators from last 7 days" do
@@ -20,18 +20,18 @@ describe "Comments ranking", type: :request do
 
     visit '/comments_ranking'
 
-    expect(page).to have_css('.ranking tr', count: 10)
-    expect(page).to have_css('.ranking tr#1', count: 1,
-                             text: top_user.name + ': ' +
-                             Comment.where(
+    expect(page).to have_css('.table tbody tr', count: 10)
+    expect(page).to have_css('.table tbody tr#1 .name', count: 1, text: top_user.name)
+    expect(page).to have_css('.table tbody tr#1 .score', count: 1,
+                             text: Comment.where(
                                user: top_user,
                                created_at: 7.days.ago..Time.now
                              ).count.to_s
                             )
 
-    expect(page).to have_css('.ranking tr#10', count: 1,
-                             text: last_rankable_user.name + ': ' +
-                             Comment.where(
+    expect(page).to have_css('.table tbody tr#10 .name', count: 1, text: last_rankable_user.name)
+    expect(page).to have_css('.table tbody tr#10 .score', count: 1,
+                             text: Comment.where(
                                user: last_rankable_user,
                                created_at: 7.days.ago..Time.now
                              ).count.to_s
